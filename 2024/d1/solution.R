9a27cf7558633bc89de66d3e34df89d73cdb4cb4
Skolmeister <alexander.nickel@mailbox.org>
2024-12-01 22:07:58 +0100

    solution to day 1

--- 2024/d1/solution.R
+++ 2024/d1/solution.R
@@ -1,1 +1,30 @@
-print("hello")
+tictoc::tic()
+input <- tibble::tibble(
+  input = readLines(here::here("2024", "d1", "input"))
+) |>
+  tidyr::separate_wider_regex(
+    input, 
+    patterns = c(list1 = "\\d+", "\\s+", list2 = "\\d+")
+  )
+
+part1 <- input |>
+  dplyr::mutate(
+    list1 = as.integer(sort(list1)),
+    list2 = as.integer(sort(list2)),
+    diff = abs(list1 - list2)
+  ) |>
+  dplyr::pull(diff) |>
+  sum()
+
+
+part2 <- input |>
+  dplyr::filter(
+    list2 %in% list1
+  ) |>
+  dplyr::count(list2) |>
+  dplyr::mutate(
+    similarity_score = as.integer(list2) * n
+  ) |>
+  dplyr::pull(similarity_score) |>
+  sum()
+tictoc::toc()
